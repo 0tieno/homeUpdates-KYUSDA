e377a6526da959640916f3a6c311ec89863a6510
Otieno <ronvi035@gmail.com>
2023-10-10 17:21:04 +0300

    added images and started slideshow functionality

--- js/main.js
+++ js/main.js
@@ -1,36 +1,9 @@
-// Define an array of image URLs for your slideshow
-const imageUrls = [
-    "images/hero - 1.JPG",
-    "images/hero-2.JPG",
-    "images/hero-3.JPG",
-    "images/hero-4.JPG",
-    // Add more image URLs as needed
-  ];
-  
-  const imgElement = document.getElementById("slideshow-img");
-  let currentIndex = 0;
-  
-  // Function to update the image source
-  function updateImage() {
-    imgElement.src = imageUrls[currentIndex];
-  }
-  
-  // Function to advance to the next image
-  function nextImage() {
-    currentIndex = (currentIndex + 1) % imageUrls.length;
-    updateImage();
-  }
-  
-  // Function to go back to the previous image
-  function prevImage() {
-    currentIndex = (currentIndex - 1 + imageUrls.length) % imageUrls.length;
-    updateImage();
-  }
-  
-  // Set an interval to automatically advance the slideshow
-  const intervalId = setInterval(nextImage, 10000); // Change images every 3 seconds (3000 milliseconds)
-  
-  // Optionally, you can add event listeners for next and previous buttons
-  document.getElementById("next-button").addEventListener("click", nextImage);
-  document.getElementById("prev-button").addEventListener("click", prevImage);
-  
+var slideIndex = 0;
+ showSlides(); 
+ function showSlides() { 
+  var i; 
+  var slides = document.getElementsByClassName("slideshow-img"); for (i = 0; i < slides.length; i++) { slides[i].style.display = "none";
+} slideIndex++; if (slideIndex > slides.length) {slideIndex = 1}
+slides[slideIndex-1].style.display = "block";
+setTimeout(showSlides, 2000); // Change image every 2 seconds 
+}
