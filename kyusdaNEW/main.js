944263e935156758171d8dc02b9b9dacce66006d
Otieno <ronvi035@gmail.com>
2023-11-05 00:18:30 +0300

    added two-digit formatting of time countdown

--- kyusdaNEW/main.js
+++ kyusdaNEW/main.js
@@ -5,6 +5,12 @@ function updateCountdown() {
   const currentDate = new Date().getTime();
   const timeRemaining = targetDate - currentDate;
 
+  if (timeRemaining <= 0) {
+    // If the target date has passed, you can display a message or take some action here.
+    document.getElementById('countdown').textContent = 'Event has started!';
+    return;
+  }
+
   const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
   const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
