5a31b8659c96889b5322275a22158f0d90d1c5ce
Otieno <ronvi035@gmail.com>
2023-11-05 12:09:33 +0300

    worked on the sticky nav

--- kyusdaNEW/app.js
+++ kyusdaNEW/app.js
@@ -25,3 +25,23 @@ function updateCountdown() {
 
   // Initial call to set the countdown when the page loads
   updateCountdown();
+
+
+
+  // When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.scrollY > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
